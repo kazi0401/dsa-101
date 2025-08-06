197a3837089cff344c2dcc258148bb4f6ace8d11
Tanzir Kazi <kazit@uci.edu>
2025-08-06 15:14:32 -0700

    Stack problems

---
+++ stack/mystack/stack.py
@@ -0,0 +1,27 @@
+# Exception Class 
+class StackException(Exception):
+    def __init__(self):
+        self.error_code = 'Improper Stack Usage!'
+    def __str__(self):
+        return f'{self.error_code}'
+    
+# Your implementation here
+class Stack:
+    def __init__(self):
+        pass 
+
+    def push(self): 
+        return 0
+
+
+
+
+# Solved xyz
+
+            
+
+
+
+
+
+    
